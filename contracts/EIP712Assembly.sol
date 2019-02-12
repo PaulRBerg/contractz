6a580ceb875ef5ef0cd370c13d0d1f08ccaf3ecb
Paul Berg <hello@paulrberg.com>
2019-02-12 17:49:44 +0000

    Fix linting issues

--- contracts/EIP712Assembly.sol
+++ contracts/EIP712Assembly.sol
@@ -1,8 +1,8 @@
-// solhint-disable var-name-mixedcase
+/* solhint-disable var-name-mixedcase */
 pragma solidity ^0.5.0;
 
 
-contract EIP712 {
+contract EIPAssembly {
 
   // EIP191 header for EIP712 prefix
   string constant internal EIP191_HEADER = "\x19\x01";
@@ -15,27 +15,17 @@ contract EIP712 {
 
   // Hash of the EIP712 Domain Separator Schema
   bytes32 constant internal EIP712_DOMAIN_SEPARATOR_SCHEMA_HASH = keccak256(abi.encodePacked(
-    "EIP712Domain(",
-      "string name,",
-      "string version,",
-      "address verifyingContract",
-    ")"
+      "EIP712Domain(",
+          "string name,",
+          "string version,",
+          "address verifyingContract",
+      ")"
   ));
 
   // Hash of the EIP712 Domain Separator data
   bytes32 private EIP712_DOMAIN_HASH;
-  bytes32 private EIP712_DOMAIN_HASH_ASSEMBLY;
-  
-  function setDomainHashSolidity() public {
-    EIP712_DOMAIN_HASH = keccak256(abi.encodePacked(
-      EIP712_DOMAIN_SEPARATOR_SCHEMA_HASH,
-      keccak256(bytes(EIP712_DOMAIN_NAME)),
-      keccak256(bytes(EIP712_DOMAIN_VERSION)),
-      bytes32(uint256(address(this)) << 96)
-    ));
-  }
-  
-  function setDomainHashAssembly() public {
+
+  constructor() public {
     assembly {
       let memPtr := mload(0x40)
       // schema = "EIP712Domain(string name,string version,address verifyingContract)"
@@ -47,15 +37,11 @@ contract EIP712 {
       // address
       mstore(add(memPtr, 0x60), address)
       // domain hash
-      sstore(EIP712_DOMAIN_HASH_ASSEMBLY_slot, keccak256(memPtr, 0x80))
+      sstore(EIP712_DOMAIN_HASH_slot, keccak256(memPtr, 0x80))
     }
   }
   
   function getDomainHash() public view returns (bytes32) {
     return EIP712_DOMAIN_HASH;
-  }
-  
-  function getDomainHashAssembly() public view returns (bytes32) {
-    return EIP712_DOMAIN_HASH_ASSEMBLY;
   }
 }
