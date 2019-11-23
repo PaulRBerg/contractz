cc3f1b77c365d9c22dc58e0a0e41b128d77c9b08
Paul Razvan Berg <hello@paulrberg.com>
2019-11-23 17:42:51 +0200

    refactor: reduce entropy by pruning the "js" folder

--- js/eth/balance.js
+++
@@ -1,9 +0,0 @@
-const config = require("../config");
-const web3 = require("../web3");
-
-web3.eth
-  .getBalance(process.argv[2] || config.accounts[0])
-  .then((data) => {
-    console.log("Eth balance is", web3.utils.fromWei(data, "ether"));
-  })
-  .catch(console.log);
